1a41a2d73a2e1d0caf376a9f7a174784fcb20c31
allperdogan <47662425+allperdogan@users.noreply.github.com>
2019-02-15 16:08:10 +0300

    greenled

--- blink.c
+++ blink.c
@@ -7,13 +7,13 @@
 void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= 0x01;					// configure P1.0 as output
+	P4DIR |= 0x80;					// configure P1.0 as output
 
 	volatile unsigned int i;		// volatile to prevent optimization
 
 	while(1)
 	{
-		P1OUT ^= 0x01;				// toggle P1.0
+		P4OUT ^= 0x80;				// toggle P1.0
 		for(i=10000; i>0; i--);     // delay
 	}
 }
